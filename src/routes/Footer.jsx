ff338e4b1718fcdcb1b5b70dac3f3b23274c907f
user <user@users-MacBook-Pro.local>
2023-08-07 00:03:35 -0700

    responsiveness added

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
             <div className="footer-container">
             <div className="footer-box">
                 <Link className="foot-link"><i className="fa-brands fa-facebook"></i></Link>
+                <Link to='https://wa.me/08062142972' className="foot-link" target="blank"><i class="fa-brands fa-whatsapp"></i></Link>
                 <Link className="foot-link"><i className="fa-brands fa-twitter"></i></Link>
                 <Link className="foot-link"><i className="fa-brands fa-linkedin"></i></Link>
                 <Link className="foot-link"><i className="fa-brands fa-github"></i></Link>
                 <Link  className="foot-link"><i class="fa-brands fa-instagram"></i></Link>
-                <Link to='https://wa.me/08062142972' className="foot-link" target="blank"><i class="fa-brands fa-whatsapp"></i></Link>
             </div>
             <p>
             &copy; 2023 WesttyCode
