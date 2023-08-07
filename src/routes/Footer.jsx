2f3377b5164001ad9c3788cd833b958240f36017
user <user@users-MacBook-Pro.local>
2023-08-06 23:40:21 -0700

    social link added to footer

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+
+
 const Footer = () => {
     return(
         <div id="footer">
+            <div className="footer-container">
+            <div className="footer-box">
+                <Link className="foot-link"><i className="fa-brands fa-facebook"></i></Link>
+                <Link className="foot-link"><i className="fa-brands fa-twitter"></i></Link>
+                <Link className="foot-link"><i className="fa-brands fa-linkedin"></i></Link>
+                <Link className="foot-link"><i className="fa-brands fa-github"></i></Link>
+                <Link  className="foot-link"><i class="fa-brands fa-instagram"></i></Link>
+                <Link to='https://wa.me/08062142972' className="foot-link" target="blank"><i class="fa-brands fa-whatsapp"></i></Link>
+            </div>
             <p>
             &copy; 2023 WesttyCode
             </p>
+            </div>
         </div>
     )
 }
